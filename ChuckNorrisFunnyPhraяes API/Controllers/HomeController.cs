cb475d388d4c4142d368249fd18d9cad034d25c5
Pavel-Iliev-1 <pvl_iliev@abv.bg>
2023-01-23 18:43:39 +0200

    Add project files.

---
+++ ChuckNorrisFunnyPhraяes API/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using ChuckNorrisFunnyPhrases_API.Models;
+using ChuckNorrisFunnyPhraяes_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ChuckNorrisFunnyPhrases_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var phrase = new Phrase();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"),
+                Headers =
+    {
+        { "accept", "application/json" },
+        { "X-RapidAPI-Key", "1415573fffmsh9674e2af201b09cp1b1186jsna63e82e106ac" },
+        { "X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                phrase = JsonConvert.DeserializeObject<Phrase>(body);
+                
+            }
+
+            ViewData.Model = phrase;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChuckNorrisFunnyPhraяes API/Models/Phrase.cs
@@ -0,0 +1,7 @@
+﻿namespace ChuckNorrisFunnyPhraяes_API.Models
+{
+    public class Phrase
+    {
+        public string Value { get; set; } = null!;
+    }
+}
